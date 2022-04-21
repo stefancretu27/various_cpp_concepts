5f81b17be794436b8cd092c5f5abb483fffb2a56
stefancretu27 <stefancretu27@gmail.com>
2022-04-21 13:49:56 +0300

    added explanations on why not specialize functions/methods + alternative solution

--- Templates/TemplateFunctions.hpp
+++ Templates/TemplateFunctions.hpp
@@ -10,6 +10,7 @@
 #include "MultipleInheritance.hpp"
 using namespace std;
 
+// Template base function with 2 template type parameters
 template<typename T, class S>
 S castTtoS(const T& t)
 {
@@ -20,7 +21,7 @@ S castTtoS(const T& t)
 	return result;
 }
 
-//specialization for bool
+// Specialization for T=bool and S=bool
 template<>
 bool castTtoS(const bool& spt)
 {
@@ -28,7 +29,8 @@ bool castTtoS(const bool& spt)
 	return false;
 }
 
-//<int, bool> is not necessary, but can be written for clarity
+// Specialization for T=int and S=bool
+// <int, bool> is not necessary due to template argument deduction, but can be written for clarity
 template<>
 bool castTtoS<int, bool>(const int& spt)
 {
@@ -36,12 +38,14 @@ bool castTtoS<int, bool>(const int& spt)
 	return false;
 }
 
-//do not specialize template functions, but use plain function as overload, as long as data type are known
+// do not specialize template functions, but use plain function as overload, as long as data types are known
 void castTtoS(bool t)
 {
 	
 }
 
+// Overload base template function for raw pointers. 
+// It might look like a partial specialization, but it's not as functions/methods cannot be partially specialized.
 template<typename T, class S>
 S* castTtoS(T* spt)
 {
@@ -71,13 +75,14 @@ S* castTtoS(T* spt)
 	
 }
 
-//template specialization for the above with types bool* and void*
+// Template specialization for the above overload for pointers with types T=bool and S=void
 template<>
 void* castTtoS(bool* spt)
 {
 	return nullptr;
 }
 
+// Overload base template function for smart pointers. 
 template<typename T, class S>
 shared_ptr<S> castTtoS(const shared_ptr<T>& spt)
 {
@@ -113,7 +118,25 @@ shared_ptr<S> castTtoS(const shared_ptr<T>& spt)
 
 
 /////////////////////////////////////////////////////////////////
+/*
+* As Herb Sutter mentioned in http://www.gotw.ca/publications/mill17.htm, template functions/methods should not
+* be specialized, as the specializations are not involved in overload resolution. The overload resolution only
+* takes into account non template implementations and base template implementations, including their overloads.
+* Additionally, functions/methods cannot be partially specialized.
+*
+* Consequently, if a suitable match is found during an overload resolution, that match will be used. 
+* If that match is specialized, ony then a specialization will be chosen. If there is specialized a base template 
+* that is considered not the best match, that specialization won't be chosen.
+*
+* However, when dealing with template functions/methods for which is desired to implement specialization, the 
+* devised strategy entails the folloeing steps
+*	1) the business logic of that template function is encapsulated as a static emthod of s struct. This way,
+* as the struct can be fully/partially specialized, the static method's implementation can be changed accordingly.
+*	2) the template function's interface/sigmnature is not altered, as it might be used by other components
+*	3) the template function's implementation only makes a call to the static method encapsulated by the struct
+*/
 
+// (1) Base template struct with template type parameter T 
 template<class T>
 struct EncapsulateStaticTemplateMethod
 {
@@ -126,7 +149,7 @@ struct EncapsulateStaticTemplateMethod
 };
 
 
-//full specialization for int, change method's signature
+// (2) Full specialization of the template struct for T=int. The method's return type can be changed.
 template<>
 struct EncapsulateStaticTemplateMethod<int>
 {
@@ -139,7 +162,8 @@ struct EncapsulateStaticTemplateMethod<int>
 	}
 };
 
-//partial specialization for pointers
+// (3) Partial specialization for raw pointers of the base template struct. 
+// This would be impossible with template functions, but would make use of overloads to overcome this downside.
 template<class T>
 struct EncapsulateStaticTemplateMethod<T*>
 {
@@ -151,6 +175,7 @@ struct EncapsulateStaticTemplateMethod<T*>
 	}
 };
 
+// (4) Full specialization of the base template for pointers to void
 template<>
 struct EncapsulateStaticTemplateMethod<void*>
 {
@@ -162,15 +187,19 @@ struct EncapsulateStaticTemplateMethod<void*>
 	}
 };
 
-//the wrapper of the static template method which performs invokation on the appropriate specialization
+
+// The wrapper of the static template method which performs invokation on the appropriate specialization
 template<class T>
 void wrapTemplateMethodCall(const T& t)
 {
+	// If T=void* the call would be solved to (4). If T is a raw pointer, the call would be solved to (3),
+	// and (2) would be called for T=int, as T would be deduced accordingly in each case and the appropriate
+	// implementation would be chosen, or generated for when (3) or (1) are considered best matches.
 	EncapsulateStaticTemplateMethod<T>::methodToBeSpecialized(t);
 }
 
-
-//demonstrate that function explicit specialization is less flexible than clas full specialization
+////////////////////////////////////////////////////////
+//demonstrate that function explicit specialization is less flexible than class full specialization
 template<class T>
 void anotherMethodToBeSpecialized(const T& t)
 {

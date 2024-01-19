73f0bdd76a31ba1916cb81fe2435e812d4e43a28
stefancretu27 <stefancretu27@gmail.com>
2024-01-19 19:32:32 +0200

    Delete Templates/TemplateClasses/TemplateClassFullSpecialization.cpp

--- Templates/TemplateClasses/TemplateClassFullSpecialization.cpp
+++
@@ -1,7 +0,0 @@
-#include "TemplateClass.hpp"
-
-//no template delcaration is further required, it is only needed when the class is declared.
-void CheckType<bool>::printType() const
-{
-    std::cout<<"type is bool"<<std::endl;
-}

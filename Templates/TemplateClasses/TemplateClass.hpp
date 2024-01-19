a1802f25c576931aa57d8c79c22fc6b40df8223d
stefancretu27 <stefancretu27@gmail.com>
2024-01-19 19:31:32 +0200

    Update TemplateClass.hpp

--- Templates/TemplateClasses/TemplateClass.hpp
+++ Templates/TemplateClasses/TemplateClass.hpp
@@ -1,35 +1,23 @@
 #pragma once
+#include <memory>
 
-#include <iostream>
-
-template<class T>
-class CheckType
+template<class S, class T>
+class TemplateClass
 {
     public:
-    void printType() const;
+    TemplateClass();
+    explicit TemplateClass(const S& s, const T& t);
+    TemplateClass(const TemplateClass& tc);
+    TemplateClass(TemplateClass&& tc);
+    TemplateClass& operator=(TemplateClass tc);
+    void swap(TemplateClass& tc);
+    //define friend swap function for being used by std algorithms
+    friend void swap(TemplateClass<S, T>& lhs, TemplateClass<S, T>& rhs);
+    
+    const std::unique_ptr<S>& GetMUptrData() const {return mUptrData;};
+    T GetMMember() const {return mMember;};
+    
+    private:
+    std::unique_ptr<S> mUptrData;
+    T mMember;
 };
-
-//full specialization for bool
-template<>
-class CheckType<bool>
-{
-    public:
-    void printType() const;
-};
-
-//partial specialization for pointers
-template <class T>
-class CheckType<T*>
-{
-    public:
-    void printType() const;
-};
-
-//partial specialization for references
-template <class T>
-class CheckType<T&>
-{
-    public:
-    void printType() const;
-};
-

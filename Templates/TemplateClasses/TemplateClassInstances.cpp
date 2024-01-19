602b774d5b1432e96493cb4e00013c766c69da57
stefancretu27 <stefancretu27@gmail.com>
2024-01-19 19:33:02 +0200

    Delete Templates/TemplateClasses/TemplateClassInstances.cpp

--- Templates/TemplateClasses/TemplateClassInstances.cpp
+++
@@ -1,7 +0,0 @@
-#include "TemplateClass.cpp"
-
-//full specializations do not require instantiation as the types are knwon
-//template class CheckType<bool>;
-template class CheckType<int>;
-template class CheckType<int&>;
-template class CheckType<int*>;

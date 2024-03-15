021d27910f059a1b8f2e4f04858c3bf4bc752b30
stefancretu27 <stefancretu27@gmail.com>
2024-03-15 17:05:53 +0200

    Update main.cpp

--- C++17/main.cpp
+++ C++17/main.cpp
@@ -7,11 +7,18 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <iostream>
+#include <complex>
+#include <array>
+#include <sstream>
+#include <chrono>
+#include <vector>
+#include <random>
 
+#include "LibraryFeatures.hpp"
 #include "LanguageFeatures.hpp"
 #include "InlineVariable.hpp"
-using namespace std;
 
+using namespace std;
 
 template<class T>
 constexpr void ff(const T& val)
@@ -178,6 +185,138 @@ int main()
     
     TemplateClass tempClass1{1,2,3};
     cout<<tempClass1.GetMember()<<endl;
+
+    std::cout<<"----------------------Library features"<<std::endl;
+    std::cout<<"----------------------std::any examples"<<std::endl;
+    auto a0 = std::make_any<std::string>("Hello, std::any!\n");
+    auto a1 = std::make_any<std::complex<double>>(0.1, 2.3);
+ 
+    std::cout << std::any_cast<std::string>(a0);
+    //cast to reference value category (property of expressions)
+    std::cout << std::any_cast<std::complex<double>&>(a1) <<std::endl;
+    //cast to ptr 
+    auto any_ptr = std::any_cast<std::string>(&a0);
+    std::cout<<*any_ptr<<std::endl;
+    //cast to reference to r-value
+    std::string str{"example string"};
+    auto rvalueref_any = std::any_cast<std::string&&>(std::move(str));
+    std::cout<<"####"<<str<<" %%%% "<<rvalueref_any<<std::endl;
+    
+    std::any anyVar{5};
+    std::cout<<"any value: "<<std::any_cast<int>(anyVar)<<" "<<anyVar.type().name()<<" "<<anyVar.has_value()<<std::endl;
+    anyVar = 3.14159;
+    std::cout<<"any value after operator=: "<<std::any_cast<double>(anyVar)<<" "<<anyVar.type().name()<<" "<<anyVar.has_value()<<std::endl;
+    anyVar.emplace<std::string>(std::string("const char*"));
+    std::cout<<"any value after emplace<T>: "<<std::any_cast<std::string>(anyVar)<<" "<<anyVar.type().name()<<" "<<anyVar.has_value()<<std::endl;
+    anyVar.reset();
+    
+    std::cout<<"----------------------std::charconv examples"<<std::endl;
+    char buffer[10];
+    int i{1234567890};
+    
+    auto [ptr, ec] = std::to_chars(buffer, buffer + sizeof(buffer), i);
+    if(ec == std::errc())
+    {
+        std::cout<<"std::to_chars"<<" "<<*ptr<<" "<<buffer<<std::endl;
+    }
+        
+    stringstream sstream;
+    sstream <<i;
+    std::cout<<"stringstream "<<sstream.str()<<std::endl;
+    
+    std::string strFloat{"3.14159"};
+    float pi{};
+    
+    std::from_chars_result result = std::from_chars(strFloat.c_str(), strFloat.c_str() + strFloat.size(), pi);
+    if(result.ec == std::errc())
+    {
+        std::cout<<pi<<" "<<*result.ptr<<" "<<pi*2<<std::endl;
+    }
+    
+    stringstream sstream2{strFloat};
+    float pi2{};
+    sstream2 >> pi2;
+    std::cout<<"stringstream float "<<pi2<<std::endl;
+    
+    std::cout<<"----------------------std::execution examples"<<std::endl;
+    std::vector<int> vecInt{9000, -33, 4, 1, 0, -47, 2020, 4007, 4, 5, 2, 101, 1001, 100, 99};
+    auto startTime = std::chrono::steady_clock::now();
+    std::sort(std::execution::par, vecInt.begin(), vecInt.end());
+    auto endTime = std::chrono::steady_clock::now();
+    std::cout<<"parallel sort time: "<<std::chrono::duration_cast<std::chrono::microseconds>(endTime-startTime).count()<<std::endl;
+    
+    startTime = std::chrono::steady_clock::now();
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(vecInt.begin(), vecInt.end(), g);
+    endTime = std::chrono::steady_clock::now();
+    std::cout<<"shuffle time: "<<std::chrono::duration_cast<std::chrono::microseconds>(endTime-startTime).count()<<std::endl;
+    
+    startTime = std::chrono::steady_clock::now();
+    std::sort(std::execution::unseq, vecInt.begin(), vecInt.end());
+    endTime = std::chrono::steady_clock::now();
+    std::cout<<"unseq sort time: "<<std::chrono::duration_cast<std::chrono::microseconds>(endTime-startTime).count()<<std::endl;
+    std::shuffle(vecInt.begin(), vecInt.end(), g);
+    
+    startTime = std::chrono::steady_clock::now();
+    std::sort(std::execution::seq, vecInt.begin(), vecInt.end());
+    endTime = std::chrono::steady_clock::now();
+    std::cout<<"seq sort time: "<<std::chrono::duration_cast<std::chrono::microseconds>(endTime-startTime).count()<<std::endl;
+    std::shuffle(vecInt.begin(), vecInt.end(), g);
+    
+    startTime = std::chrono::steady_clock::now();
+    std::sort(std::execution::par_unseq, vecInt.begin(), vecInt.end());
+    endTime = std::chrono::steady_clock::now();
+    std::cout<<"par_unseq sort time: "<<std::chrono::duration_cast<std::chrono::microseconds>(endTime-startTime).count()<<std::endl;
+    std::shuffle(vecInt.begin(), vecInt.end(), g);
+    
+    startTime = std::chrono::steady_clock::now();
+    auto pivot = vecInt[8]; 
+    std::cout<<"pivot: "<<pivot<<std::endl;
+    std::partition(std::execution::par_unseq, vecInt.begin(), vecInt.end(), [pivot](const int val){return val<pivot;});
+    endTime = std::chrono::steady_clock::now();
+    std::cout<<"par_unseq partition time: "<<std::chrono::duration_cast<std::chrono::microseconds>(endTime-startTime).count()<<std::endl;
+    for(auto elem : vecInt)
+    {
+        cout<<elem<<" ";
+    }
+    std::cout<<std::endl;
+    std::shuffle(vecInt.begin(), vecInt.end(), g);
+    
+    startTime = std::chrono::steady_clock::now();
+    pivot = vecInt[10];
+    std::cout<<"pivot: "<<pivot<<std::endl;
+    std::partition(std::execution::par_unseq, vecInt.begin(), vecInt.end(), [pivot](const int val){return val<pivot;});
+    endTime = std::chrono::steady_clock::now();
+    std::cout<<"unseq partition time: "<<std::chrono::duration_cast<std::chrono::microseconds>(endTime-startTime).count()<<std::endl;
+    for(auto elem : vecInt)
+    {
+        cout<<elem<<" ";
+    }
+    std::cout<<std::endl;
+    std::shuffle(vecInt.begin(), vecInt.end(), g);
+    
+    std::cout<<"----------------------std::filesystem examples"<<std::endl;
+    std::filesystem::path file_path{R"(C:\Users\scretu\Downloads\test.docx)"};
+    
+    std::cout<<"root_name: "<<file_path.root_name()<<" root_DIR: "<<file_path.root_directory()<<" root_path "<<file_path.root_path()<<endl;
+    std::cout<<"relative_path: "<<file_path.relative_path()<<" parent_path: "<<file_path.parent_path()<<" filename() "<<file_path.filename()<<endl;
+    std::cout<<"stem: "<<file_path.stem()<<" extension: "<<file_path.extension()<<" empty "<<file_path.empty()<<endl;
+    
+    const std::filesystem::path from{"/none1/a"}, to{"/none2/b"};
+    try
+    {
+        std::filesystem::copy_file(from, to); // throws: files do not exist
+    }
+    catch (std::filesystem::filesystem_error const& ex)
+    {
+        std::cout <<endl<< "what():  " << ex.what() << '\n'
+                  << "path1(): " << ex.path1() << '\n'
+                  << "path2(): " << ex.path2() << '\n'
+                  << "code().value():    " << ex.code().value() << '\n'
+                  << "code().message():  " << ex.code().message() << '\n'
+                  << "code().category(): " << ex.code().category().name() << '\n';
+    }
     
 
     return 0;

e66ba5adcf307f6ef82d5e33e84e88d04a678cdf
stefancretu27 <stefancretu27@gmail.com>
2025-05-15 15:16:03 +0300

    Update main.cpp

--- Polymorphism_Virtuality/main.cpp
+++ Polymorphism_Virtuality/main.cpp
@@ -67,8 +67,8 @@ int main()
 	cout<<"     	- same parameters types and same number of parameters"<<endl;
 	cout<<"     	- same constness."<<endl<<endl;
 
-	cout<<"     2. Dynamic polymorphism is the ability of a method call to be resolved to the most Derived implementation of that method. Polymorphism is "<<endl;
-    cout<<"     achieved as follows:"<<endl;
+	cout<<"     2. Dynamic polymorphism is the ability of a method call to be resolved to the most derived implementation of that method."<<endl;
+    	cout<<"     Dynamic polymorphism is achieved as follows:"<<endl;
 	cout<<"		- the method is declared as <<virtual>> in the most Base class"<<endl;
 	cout<<"     	- the method is overriden in Derived classes, so it has different implementations in the inheritance chain. "<<endl;
 	cout<<"     	- a pointer or reference to Base class points/refer to a Derived class and the virtual method is invoked via this pointer/ref."<<endl;
